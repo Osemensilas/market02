cdb004247b757d632c9ae551babc4eed6e55fab6
unknown <osemensilas@gmail.com>
2026-05-19 18:14:37 +0100

    New Commit

--- script/observer.js
+++ script/observer.js
@@ -1,14 +1,43 @@
 const elements = document.querySelectorAll('.hidden');
+const counter = document.getElementById("counter");
+const card = document.getElementById("counterCard");
+
+let hasStarted = false;
+
+function animateCounter(end, duration) {
+  let start = 0;
+  const startTime = performance.now();
+
+  function update(currentTime) {
+    const elapsed = currentTime - startTime;
+    const progress = Math.min(elapsed / duration, 1);
+
+    // easing (smooth acceleration + slow end)
+    const eased = 1 - Math.pow(1 - progress, 3);
+
+    const value = Math.floor(eased * end);
+    counter.textContent = value;
+
+    if (progress < 1) {
+      requestAnimationFrame(update);
+    } else {
+      counter.textContent = end;
+    }
+  }
+
+  requestAnimationFrame(update);
+}
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting){
-            console.log(entry.target);
             entry.target.classList.add("show");
+            card.classList.add("show");
+            animateCounter(4627, 1800);
         }
     });
 });
 
 elements.forEach((element) => {
-    observer.observe(element);
+    observer.observe(element, card);
 });
